3e39b70f909b8435048b17751a77f7fdfcf3daa9
wr858585 <60288327+wr858585@users.noreply.github.com>
2020-01-31 00:09:30 +1100

    Add content for Code2_Multiplying_Strings.py

--- Python_For_Kids/Code2_Multiplying_Strings.py
+++ Python_For_Kids/Code2_Multiplying_Strings.py
@@ -1,2 +1,15 @@
 #   Multiplying Strings
-#   To display strings multiple times
+#   To display strings multiple times by using * before or after strings
+
+spaces = '' * 25
+print('%s 111 Wuchuan Rd' % spaces)
+print('%s Yangpu' % spaces)
+print('%s Shanghai 200433' % spaces)
+print()
+print()
+print('Hi there')
+print('Hope you are well. I wish to report that there is an alien in the library, reading Don Quixote de la Mancha.')
+print('Do you think whether he has any idea that we understand the dark forest?')
+print()
+print('Thanks and regards')
+print('Chao')
