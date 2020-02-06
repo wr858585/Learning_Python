5290d57dabb1b3c1257b11a76d2f92ae3d7a5ec8
oono <andychao3210@gmail.com>
2020-02-06 22:44:47 +1100

    Add comments on operations

--- Python_100_days/Day02_Variables/P3_Basic Operations.py
+++ Python_100_days/Day02_Variables/P3_Basic Operations.py
@@ -6,3 +6,5 @@ a += b
 a *= a + 2
 print(a)
 
+#   += means to name the new variable a consisting of addiction of previous a and variable b
+#   Likewise, *= names the latest variable a equivalent to the product of previous variable a and (a + 2)
