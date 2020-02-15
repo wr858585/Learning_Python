dc3fad4faa753bd7e872e9633cca87c12e065b13
oono <andychao3210@gmail.com>
2020-02-15 20:35:41 +1100

    2020.2.15 commits

---
+++ Aliyun_Python/Practice/4_Odds_or_Even.py
@@ -0,0 +1,10 @@
+
+# Question: Determine whether the number that user types is an odds or even.
+
+number = int(input('Please type a number: '))
+
+if number % 2 == 0:
+    print('It is an even.')
+
+else:
+    print('It is an odds.')
---
+++ Aliyun_Python/Practice/8_Marry_Daughter.py
@@ -0,0 +1,22 @@
+
+# Question: Help mother Lisa develop an automatic filter for selecting perspective husband for her daughter, Elsa.
+# Selecting criteria are as follows:
+#   1. He should be over 180cm in height.
+#   2. He should have assets over 10 million dollars.
+#   3. He should be good looking as well with over 500 'charming points'.
+# As Elsa gets older, she still cannot marry a guy due to such picky and rigid criteria. Eventually, Lisa compromised
+# and told Elsa that she could still marry someone who possesses at least one character mentioned above whilst the
+# ideal husband have all three.
+
+height = int(input('Height (in cm): '))
+wealth = float(input('Assets (in millions): '))
+charming_points = int(input('Charming Points: '))
+
+if height >= 180 and wealth >= 10 and charming_points >= 500:
+    print('You are the perfect one.')
+
+elif height >= 180 or wealth >= 10 or charming_points >= 500:
+    print('You are tolerably accepted.')
+
+else:
+    print('Sorry you are out.')
