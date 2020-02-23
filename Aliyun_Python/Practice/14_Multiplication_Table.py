667f66cd6a26da4930b30d2febbf8293d1f024ca
oono <andychao3210@gmail.com>
2020-02-23 21:27:38 +1100

    2020.2.23 commits

--- Aliyun_Python/Practice/14_Multiplication_Table.py
+++ Aliyun_Python/Practice/14_Multiplication_Table.py
@@ -1,6 +1,8 @@
 
 # Question: Print the multiplication table.
 
+# Solution 1: Use placeholders
+
 i = 0
 while i < 9:
     i += 1
@@ -8,4 +10,18 @@ while i < 9:
     while j < i:
         j += 1
         print("%d * %d =" % (j, i), i * j, end='    ')
+    print()
+
+# Blank between solutions
+print()
+
+# Solution 2: Use format string
+
+i = 0
+while i < 9:
+    i += 1
+    j = 0
+    while j < i:
+        j += 1
+        print(f'{j} * {i} = {i * j}', end='    ')
     print()
