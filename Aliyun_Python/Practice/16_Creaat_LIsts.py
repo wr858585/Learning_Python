de1dabd07577410e1739d696a4c1691999b3f6b6
oono <andychao3210@gmail.com>
2020-02-27 20:48:19 +1100

    2020.2.26 commits

--- Aliyun_Python/Practice/16_Creaat_LIsts.py
+++
@@ -1,7 +0,0 @@
-
-# Question: Create a list that contains my friend's names.
-
-friends = ['Z', 'Lulu', 'Daqing', 'Zhouyi', 'Wood', 'Dawei', 'Dashen']
-
-print(len(friends))
-print(friends[1])
---
+++ Aliyun_Python/Practice/16_Creaat_Lists.py
@@ -0,0 +1,29 @@
+
+# Question: Create a list that contains my friend's names.
+
+friends = ['Z', 'Lulu', 'Daqing', 'Zhouyi', 'Wood', 'Dawei', 'Dashen']
+
+print(len(friends))
+print(friends[1])
+
+animal = 'dolphin'
+
+print('d' in animal)
+print('do' in animal)
+print('don' in animal)
+
+print(len(animal))
+
+print(animal[0: 1])     # 'd'
+print(animal[0: 2])     # 'do'
+print(animal[0: 2: 1])  # 'do'
+print(animal[0: 2: 2])  # 'd'
+print(animal[0: 5: 2])  # 'dlh' (not 'dl')
+
+print(animal.index('l'))  # 'l'
+print(animal.index('l', 2))  # 2
+print(animal.index('l', 2, 3))  # 2
+
+print(animal.count('n'))
+
+# (lines 14-17) remember to use colon instead of comma in brackets when slicing.
