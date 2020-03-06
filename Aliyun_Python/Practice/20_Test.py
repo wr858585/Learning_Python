5d624f836234d4858e8605deb15424af78ef00e1
oono <andychao3210@gmail.com>
2020-03-06 17:42:31 +1100

    2020.3.5 commits

--- Aliyun_Python/Practice/20_Test.py
+++ Aliyun_Python/Practice/20_Test.py
@@ -1,4 +1,3 @@
-
 # 1. The Basics
 print('-' * 50)
 print('1. The Basics')
@@ -13,9 +12,9 @@ print(my_tuple[1])
 nums = [1, 2, 3, 4, 5]
 print('原版本：', nums, id(nums))
 nums[0] = 5
-print('第一次修改：', nums, id(nums))     # Slicing does not change physical id of object in RAM.
+print('第一次修改：', nums, id(nums))  # Slicing does not change physical id of object in RAM.
 nums = [6, 7, 8, 9, 10]
-print('第二次修改：', nums, id(nums))     # It assigns another object to variable a thus changing the id.
+print('第二次修改：', nums, id(nums))  # It assigns another object to variable a thus changing the id.
 
 print('-' * 50)
 
@@ -31,7 +30,32 @@ print('d =', d)
 print('e =', e)
 
 a, b = b, a
+print('a =', a)
+print('b =', b)
+
 # It is more complicated than it seems. The left side is 2 variables whilst the right side is actually a tuple which is
 # equivalent to (b, a) as parentheses can be omitted as long as the comma is there telling python that it meas tuple.
 
+a, b, *c = nums
+print('a =', a)
+print('b =', b)
+print('c =', c)     # It returns a list as unpack is a common operation of sequence.
+
+a, *b, c = nums
+print('a =', a)
+print('b =', b)
+print('c =', c)
+
+# List, string and other sequence types can also be unpacked.
+
+my_list = [1, 2, 3, 4]
+*a, b, c = my_list
+print('a =', a)
+print('b =', b)
+print('c =', c)
+
+a, *b, c = 'hello world'
+print('a =', a)
+print('b =', b)
+print('c =', c)
 
