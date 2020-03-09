7f100528174c5fe5b513de0eed9a2bc91e0cae55
oono <andychao3210@gmail.com>
2020-03-09 19:10:21 +1100

    2020.3.9 commits

--- Aliyun_Python/Practice/22_Functions.py
+++ Aliyun_Python/Practice/22_Functions.py
@@ -17,3 +17,24 @@ def welcome(a):
 
 welcome('大野')
 
+# Task 3: The difference between changing variables and changing objects.
+
+
+def fn(a):
+    a = 20
+    print('a =', a)
+
+
+c = 10
+fn(c)
+print('c =', c)
+
+
+def fn2(a):
+    a[0] = 30
+    print('a =', a)
+
+
+c = [1, 2, 3]
+fn2(c)
+print('c =', c)
