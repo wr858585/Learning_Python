8cb2ef8dd48f76e7db9539975504c6aa7f2c0e29
oono <andychao3210@gmail.com>
2020-04-02 17:35:23 +1100

    2020.4.2 commits

--- Aliyun_Python/Practice/28_try.py
+++ Aliyun_Python/Practice/28_try.py
@@ -1,11 +1,24 @@
-# Task: Use try statement
+# Task: Try to use try statement
 
 print('hello')
+
 try:
     print(10 / 0)
 except:
     print('哈哈哈，出错啦')
 else:
     print('程序正常，可以运行')
+
 print('bye')        # bye会照常执行，程序不会退出。这就是try语句的意义
 
+print('-' * 100)
+
+print('异常出现前')
+
+try:
+    print(c)
+    print(10/0)
+except NameError:
+    print('出现NameError异常')
+
+print('异常出现后')
