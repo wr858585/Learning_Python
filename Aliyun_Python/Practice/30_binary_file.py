6d780f551085f6f0cf8fa9e5cffc7d4948d1ee0e
oono <andychao3210@gmail.com>
2020-04-06 14:18:23 +1000

    2020.4.6 commits

---
+++ Aliyun_Python/Practice/30_binary_file.py
@@ -0,0 +1,13 @@
+# Task: Get familiar with binary file operations
+
+file_name = 'demo_binary.jpg'
+
+with open(f'../demo/{file_name}', 'rb') as file_obj:
+    new_name = 'demo_2.jpg'
+    with open(new_name, 'wb') as new_obj:
+        chunk = 1024 * 100
+        while True:
+            content = file_obj.read(chunk)
+            new_obj.write(content)
+            if not content:
+                break
