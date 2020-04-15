87cfa28fb60d80a992bbfa4563f8d5ccda22f3c1
oono <andychao3210@gmail.com>
2020-04-15 21:39:50 +1000

    2020.4.15 commits

--- Aliyun_Python/Practice/30_binary_file.py
+++ Aliyun_Python/Practice/30_binary_file.py
@@ -5,7 +5,7 @@ file_name = 'demo_binary.jpg'
 with open(f'../demo/{file_name}', 'rb') as file_obj:
     new_name = 'demo_2.jpg'
     with open(new_name, 'wb') as new_obj:
-        chunk = 1024 * 100          # 1024 bytes * 100 == 100 kb
+        chunk = 1024 * 100          # 1024 bytes * 100 == 100 kb per chunk
         while True:
             content = file_obj.read(chunk)
             new_obj.write(content)
